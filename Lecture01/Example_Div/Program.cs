1d45427b17d3b8ee74d3d8d32ba173f3348bbe02
Viktor <viktor.belebezyev@yandex.ru>
2022-09-14 12:16:38 +0300

    Add Example_Div

---
+++ Lecture01/Example_Div/Program.cs
@@ -0,0 +1,13 @@
+﻿//задаем значиния 2 переменных
+
+double number_A = 20;
+double number_B = 7;
+
+//ищем частное
+
+double result = number_A / number_B;
+
+//вывод частного
+
+Console.Write("Частное 2 чисел равно - ");
+Console.Write(result);
