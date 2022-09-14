22f1c923939bbacc81304c41c3789dc658cd0a09
Viktor <viktor.belebezyev@yandex.ru>
2022-09-14 12:31:49 +0300

    edit Random

--- Lecture01/Example_Sum/Program.cs
+++ Lecture01/Example_Sum/Program.cs
@@ -3,9 +3,11 @@
 int number_A = new Random().Next(1, 10); // диапазон случайного числа от 1 до 9 включительно
 Console.Write("Первое число равно - ");
 Console.Write(number_A);
+Console.WriteLine();
 int number_B = new Random().Next(1, 10);
 Console.Write("Второе число равно - ");
 Console.Write(number_B);
+Console.WriteLine();
 
 //ищем их сумму
 
