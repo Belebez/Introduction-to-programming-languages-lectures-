b684b1c188f19265488cbb688f7d56f21e29f015
Viktor <viktor.belebezyev@yandex.ru>
2022-09-14 12:06:15 +0300

    Add Example_Sum

---
+++ Lecture01/Example_Sum/Program.cs
@@ -0,0 +1,13 @@
+﻿//задаем значиния 2 переменных
+
+int number_A = 34;
+int number_B = 34;
+
+//ищем их сумму
+
+int result = number_A + number_B;
+
+//вывод суммы
+
+Console.Write("Сумма 2 чисел равна - ");
+Console.Write(result);
