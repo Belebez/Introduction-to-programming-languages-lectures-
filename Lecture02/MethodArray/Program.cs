ef999d9a894aa2e87d55e8abb6855b9d8021b05c
Viktor <viktor.belebezyev@yandex.ru>
2022-09-19 09:01:37 +0300

    add solution algorithm

--- Lecture02/MethodArray/Program.cs
+++ Lecture02/MethodArray/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+int n = array.Length; // длина массива
+
+int find = 8; // переменная для поиска нужного числа в массиве
+
+int index = 0;
+
+while (index > n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // остановим поиск после первого нахождения нужного числа
+    }
+    index++;
+}
