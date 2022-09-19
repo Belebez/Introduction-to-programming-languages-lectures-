b8373346a2f02b824b0289ca968913a33f0bfe49
Viktor <viktor.belebezyev@yandex.ru>
2022-09-19 09:34:42 +0300

    add solution algorithm

--- Lecture02/ArrayLibrary/Program.cs
+++ Lecture02/ArrayLibrary/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// метод void не возвращает значение, т.е мы не используем return
+
+void FillArray(int[] collection)  // метод заполнения массива случайными цифрами
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1,10);  // на место индекса помещаем случайное число от 1 до 10
+        index++;
+    }
+}
+
+void PrintArray(int[] col)  // метод вывода массива
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)  // функция для поиска нужного числа
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;  // используем -1 для того чтобы, если в массиве не будет нужного нам числа, метод вернул нам это значение, если используем ноль, то это есть значение в массиве, что нам не нужно
+    while(index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создай новый масиив, в котором будет 10 элементов(по умолчанию он будет заполнен нулями)
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 5);
+Console.WriteLine(pos);
